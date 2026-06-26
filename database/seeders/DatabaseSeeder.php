7323e0f3714f2ed190ebd19ee1bdc8eb919d9827
milawati <milawati.240604017@student.hamzanwadi.ac.id>
2026-06-26 16:53:53 +0800

    feat:menyelesaikan seeder kategori

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category; 
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Category::create(['nama_kategori' => 'Prewedding']);
+        Category::create(['nama_kategori' => 'Wisuda / Kelulusan']);
+        Category::create(['nama_kategori' => 'Foto Keluarga']);
+        Category::create(['nama_kategori' => 'Pas Foto / Reguler']);
     }
 }
